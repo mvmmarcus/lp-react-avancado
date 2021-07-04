434a61e451724daa113f6005d0606d7796efc8c3
Marcus Vinicius Marques <marcus.vinicius.marques@hotmail.com>
2021-07-04 18:34:15 -0300

    feat: add graphql client

---
+++ src/graphql/queries/getLandingPage.ts
@@ -0,0 +1,151 @@
+const GET_LANDING_PAGE = /* GraphQL */ `
+  fragment image on UploadFile {
+    alternativeText
+    url
+  }
+
+  fragment logo on LandingPage {
+    logo {
+      ...image
+    }
+  }
+
+  fragment header on LandingPage {
+    header {
+      title
+      description
+      button {
+        label
+        url
+      }
+      image {
+        ...image
+      }
+    }
+  }
+
+  fragment sectionAboutProject on LandingPage {
+    sectionAboutProject {
+      title
+      description
+      image {
+        ...image
+      }
+    }
+  }
+
+  fragment sectionTech on LandingPage {
+    sectionTech {
+      title
+      techIcons {
+        title
+        icon {
+          url
+        }
+      }
+    }
+  }
+
+  fragment sectionConcepts on LandingPage {
+    sectionConcepts {
+      title
+      concepts {
+        title
+      }
+    }
+  }
+
+  fragment sectionModules on LandingPage {
+    sectionModules {
+      title
+      modules {
+        title
+        subtitle
+        description
+      }
+    }
+  }
+
+  fragment sectionAgenda on LandingPage {
+    sectionAgenda {
+      title
+      description
+    }
+  }
+
+  fragment pricingBox on LandingPage {
+    pricingBox {
+      totalPrice
+      numberInstallments
+      priceInstallment
+      benefits
+      button {
+        label
+        url
+      }
+    }
+  }
+
+  fragment sectionAboutUs on LandingPage {
+    sectionAboutUs {
+      title
+      authors {
+        photo {
+          ...image
+        }
+        name
+        role
+        socialLinks {
+          title
+          url
+        }
+        description
+      }
+    }
+  }
+
+  fragment sectionReviews on LandingPage {
+    sectionReviews {
+      title
+      reviews {
+        id
+        name
+        text
+        photo {
+          url
+        }
+      }
+    }
+  }
+
+  fragment sectionFaq on LandingPage {
+    sectionFaq {
+      title
+      questions {
+        question
+        answer
+      }
+      extraQuestions {
+        question
+        answer
+      }
+    }
+  }
+
+  query GET_LANDING_PAGE {
+    landingPage {
+      ...logo
+      ...header
+      ...sectionAboutProject
+      ...sectionTech
+      ...sectionConcepts
+      ...sectionModules
+      ...sectionAgenda
+      ...pricingBox
+      ...sectionAboutUs
+      ...sectionReviews
+      ...sectionFaq
+    }
+  }
+`
+export default GET_LANDING_PAGE
